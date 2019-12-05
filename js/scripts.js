0c1fef03e9d1bf987fd80b175efb890a4267760d
Derrick <derrickip34@gmail.com>
2019-12-05 21:07:51 +0300

    Show what we do whe the icons are clicked on

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".design").click(function() {
+        $(".hide1").toggle()
+        $(".img1").toggle()
+    })
+    $(".development").click(function() {
+        $(".hide2").toggle()
+        $(".img2").toggle()
+    })
+    $(".project").click(function() {
+        $(".hide3").toggle()
+        $(".img3").toggle()
+    })
+})
